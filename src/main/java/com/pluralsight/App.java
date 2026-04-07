442a982abeaa4f77556153db2c7711c6d718cc46
JammingJams <noahlogan20o00@gmail.com>
2026-04-07 10:55:53 -0400

    Added main method and sout

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.SQLOutput;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Testing to see if this works?");
+    }
 }
