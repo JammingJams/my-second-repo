7a9f5bdb2d9b8da91b2cbde6c35306ed7ea96a9e
JammingJams <noahlogan20o00@gmail.com>
2026-04-07 10:59:45 -0400

    Change 1st sout into A nice little message. Added a new sout

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,8 +1,5 @@
 package com.pluralsight;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.sql.SQLOutput;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("A nice little message");
