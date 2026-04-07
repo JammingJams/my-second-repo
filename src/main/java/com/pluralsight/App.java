86d78ab04dd5c54c91d826f74d7257c5ea48d6d2
JammingJams <noahlogan20o00@gmail.com>
2026-04-07 11:08:31 -0400

    Added a new tax system

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,5 +4,18 @@ public class App {
     public static void main(String[] args) {
         System.out.println("A nice little message");
         System.out.println("One more");
+
+        var price = 10;
+        var tax = 3F;
+
+        if (price > 10) {
+            System.out.println("Item is too expensive");
+        }
+        else
+            if (tax > 2) {
+                System.out.println("Tax is too much");
+            }
+            else
+                System.out.println(tax * price);
     }
 }
