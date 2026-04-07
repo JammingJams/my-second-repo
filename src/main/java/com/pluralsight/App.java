3ee1d4a44ef5d397c23b505629f2bb6918c72ba2
JammingJams <noahlogan20o00@gmail.com>
2026-04-07 10:59:29 -0400

    Change 1st sout into A nice little message. Added a new sout

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -5,6 +5,7 @@ import java.sql.SQLOutput;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Testing to see if this works?");
+        System.out.println("A nice little message");
+        System.out.println("One more");
     }
 }
